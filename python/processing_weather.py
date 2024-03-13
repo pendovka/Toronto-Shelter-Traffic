e5eb95adc85218090c2691d8d8e8dda9e76857d1
pendovka <arkhangelskiy.alex@gmail.com>
2024-03-13 00:48:14 -0400

    -

--- python/processing_weather.py
+++ python/processing_weather.py
@@ -2,26 +2,34 @@ import pandas as pd
 from io import StringIO
 import requests
 
-csv_url = 'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=51459&Year=2023&Month=3&Day=1&time=&timeframe=2&submit=Download+Data'
+# URLs for CSV data
+csv_urls = {
+    '2023': 'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=51459&Year=2023&Month=3&Day=1&time=&timeframe=2&submit=Download+Data',
+    '2022': 'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=51459&Year=2022&Month=3&Day=1&time=&timeframe=2&submit=Download+Data',
+    '2021': 'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=51459&Year=2021&Month=3&Day=1&time=&timeframe=2&submit=Download+Data'
+}
 
-response = requests.get(csv_url)
+weather_data_frames = []
 
-# Check if the request was successful
-if response.ok:
-    # Use StringIO to convert the text data into a file-like object so that pandas can read it
-    data = StringIO(response.text)
-    
-    # Read the data into a pandas DataFrame
-    weather = pd.read_csv(data)
-    
-    weather = weather.rename(columns={'Date/Time': 'Date', 'Min Temp (°C)': 'min_temp_cels', 'Total Precip (mm)': 'total_precip_mm'})
+for year, url in csv_urls.items():
+    response = requests.get(url)
+    if response.ok:
+        data = StringIO(response.text)
+        weather_df = pd.read_csv(data)
+        weather_df = weather_df.rename(columns={
+            'Date/Time': 'Date',
+            'Min Temp (°C)': 'min_temp_cels'
+        })
+        weather_df['Date'] = pd.to_datetime(weather_df['Date'])
+        weather_df.set_index('Date', inplace=True)
+        weather_df = weather_df[['min_temp_cels']]
+        weather_df['min_temp_cels'] = weather_df['min_temp_cels'].interpolate(method='linear')
+        
+        weather_data_frames.append(weather_df)
+    else:
+        print(f'Failed to retrieve data for {year}. Status code: {response.status_code}')
 
-    weather['Date'] = pd.to_datetime(weather['Date']) 
-    weather.set_index('Date', inplace=True)
+concatenated_weather = pd.concat(weather_data_frames)
+concatenated_weather.sort_index(ascending=True, inplace=True)
 
-    weather = weather[['min_temp_cels', 'total_precip_mm']]
-    weather['min_temp_cels'] = weather['min_temp_cels'].interpolate(method='linear')
-    weather['total_precip_mm'] = weather['total_precip_mm'].interpolate(method='linear')
-
-else:
-    print(f'Failed to retrieve data. Status code: {response.status_code}')
+print(concatenated_weather)
