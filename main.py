6e6bc2caa5d79238d3cba206105092a10763ea48
pendovka <arkhangelskiy.alex@gmail.com>
2024-03-26 22:21:16 -0400

    commit

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from flask import Flask, send_file, url_for, render_template
-from model import plot_predictions  
-import matplotlib.pyplot as plt
-import os
-from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask
 
 app = Flask(__name__)
 
 i = 0
+b = 0
 
 @app.route('/')
 def index():
