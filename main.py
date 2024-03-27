c9e990b8fcc1b44c11b22e87f089622e2b8fcde1
pendovka <arkhangelskiy.alex@gmail.com>
2024-03-26 22:14:28 -0400

    -

--- main.py
+++ main.py
@@ -6,33 +6,12 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
-def save_plot():
-    print("save_plot() function called.")
-    print("Current working directory:", os.getcwd())
-    
-    plt.figure(figsize=(15, 7))
-    plot_predictions() 
-    plt.savefig('static/predictions.png')  # Save in static directory
-    plt.close()
-    print("Plot saved to:", os.path.abspath('static/predictions.png'))
-
-# Initialize the scheduler with your preferred settings.
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=save_plot, trigger="interval", hours=1)
-scheduler.start()
+i = 0
 
 @app.route('/')
 def index():
-    # Provide a link to the predictions plot or embed it in an HTML page.
-    return render_template('index.html', plot_url=url_for('predictions'))
+    
+    i +=  1
 
-@app.route('/predictions')
-def predictions():
-    # Return the plot image file in the response
-    return send_file('static/predictions.png', mimetype='image/png')
+    return i
 
-if __name__ == '__main__':
-    if not os.path.isfile('static/predictions.png'):
-        save_plot()
-    scheduler.start()  # Start the scheduler when running the app directly
-    app.run(debug=True)
